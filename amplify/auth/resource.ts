9de50bf007868c0b7f3ddf75e73ff9821264389f
terae7 <terae7@gmail.com>
2026-02-16 16:43:11 -0500

    bucket list tracker app

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,23 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
+
+const schema = a.schema({
+  BucketItem: a
+    .model({
+      title: a.string(),
+      description: a.string(),
+      image: a.string(),
+    })
+    .authorization((allow) => [allow.owner()]),  // Restrict access to the owner
+});
+
+
+export type Schema = ClientSchema<typeof schema>;
+
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'userPool',
   },
 });
---
+++ amplify/resource.ts
@@ -0,0 +1,12 @@
+
+import { defineStorage } from "@aws-amplify/backend";
+
+
+export const storage = defineStorage({
+  name: "amplifyBucketTrackerImages",
+  access: (allow) => ({
+    "media/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+  }),
+});
